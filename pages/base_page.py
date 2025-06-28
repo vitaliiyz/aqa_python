5b357c16e23d31db0df523f7032c1a906e66b942
Evgenii <evg.bazhanov@yahoo.com>
2025-06-28 16:01:55 +0200

    move selectors inside

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import expect
 
+
 class BasePage:
     def __init__(self, page):
         self._url = None
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,4 @@
-from .base_page import BasePage
-
-popup_locator = '[class*="CookiesConsentsBannerRodoHelper"]'
-popup_text = "Prywatność Użytkownika"
-desktop_buttons_locator = '[class*="buttonsDesktop"]'
-accept_button_locator = (
-    '//div[contains(@class, "buttonsDesktop")]/button[@data-ta="cookie-btn-accept-all"]'
-)
-user_menu_locator = '[class="empikNav__userLink userMenu"]'
-register_link_locator = '[title="Zarejestruj się"]'
+from pages.base_page import BasePage
 
 
 class MainPage(BasePage):
@@ -16,19 +7,21 @@ class MainPage(BasePage):
         self._url = "https://www.empik.com/"
 
     def popup(self):
-        return self.find_by_locator(popup_locator)
+        return self.find_by_locator('[class*="CookiesConsentsBannerRodoHelper"]')
 
     def popup_text_element(self):
-        return self.find_by_text(popup_text)
+        return self.find_by_text("Prywatność Użytkownika")
 
     def desktop_buttons(self):
-        return self.find_by_locator(desktop_buttons_locator)
+        return self.find_by_locator('[class*="buttonsDesktop"]')
 
     def accept_button(self):
-        return self.find_by_locator(accept_button_locator)
+        return self.find_by_locator(
+            '//div[contains(@class, "buttonsDesktop")]/button[@data-ta="cookie-btn-accept-all"]'
+        )
 
     def user_menu(self):
-        return self.find_by_locator(user_menu_locator)
+        return self.find_by_locator('[class="empikNav__userLink userMenu"]')
 
     def register_link(self):
-        return self.find_by_locator(register_link_locator)
+        return self.find_by_locator('[title="Zarejestruj się"]')
